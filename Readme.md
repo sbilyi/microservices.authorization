4b6cb5adc9e2dba0bef3fa9424d0f6602018f839
Sergii Bilyi <kartanisoid@gmail.com>
2019-08-07 00:48:37 +0300

    user controller controller added

--- Readme.md
+++ Readme.md
@@ -4,4 +4,12 @@ $ curl -X POST -d '{"name":"sergii", "username":"kartanis", "email":"kartanisoid
 
 ## login
 $ curl -X POST -d '{"usernameOrEmail":"kartanisoid@gmail.com", "password": "secret123"}' -H "Content-Type: application/json" localhost:8080/api/auth/signin
+
+## current user info
+$ curl -X GET \
+    -H "Content-Type: application/json"\
+    -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTY1MTI3OTU4LCJleHAiOjE1NjU3MzI3NTh9.Pe0RvhLzXByJQlWqESRXe3_86ui3SVTj9w8pfL_0e-rnlUeuOfCOVRWhtfDwATijdsvZaau1tvniyffPE9dXsw" \
+    localhost:8080/api/user/me
+
+eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTY1MTI3OTU4LCJleHAiOjE1NjU3MzI3NTh9.Pe0RvhLzXByJQlWqESRXe3_86ui3SVTj9w8pfL_0e-rnlUeuOfCOVRWhtfDwATijdsvZaau1tvniyffPE9dXsw"
 ``` 
---
+++ src/main/java/com/sbilyi/microservices/authorization/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.sbilyi.microservices.authorization.controller;
+
+import com.sbilyi.microservices.authorization.model.UserPrincipal;
+import com.sbilyi.microservices.authorization.payload.UserSummary;
+import com.sbilyi.microservices.authorization.repository.UserRepository;
+import com.sbilyi.microservices.authorization.service.CurrentUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+}
